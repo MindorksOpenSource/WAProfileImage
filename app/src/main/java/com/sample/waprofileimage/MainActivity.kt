83e07139b3606788606ec9ea89432d56db1679ef
mohammed mahmoud <mohammed.95mahmoud@gmail.com>
2018-09-04 17:20:54 +0200

    test custom dialog

--- app/src/main/java/com/sample/waprofileimage/MainActivity.kt
+++ app/src/main/java/com/sample/waprofileimage/MainActivity.kt
@@ -2,13 +2,19 @@ package com.sample.waprofileimage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mindorks.waprofileimage.waprofileImage.WAProfileImage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        WAProfileImage.launch(this,105)
+        findViewById<View>(R.id.sample_image).setOnClickListener { v ->
+            WAProfileImage.launch(this,105)
+
+        }
+
 
     }
 }
